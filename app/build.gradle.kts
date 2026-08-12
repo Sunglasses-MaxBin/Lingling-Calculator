7afb6bbd3ff05f7f13faf451f4bda0ee3d7021ff
Sunglasses-MaxBin <Sunglasses0920@outlook.com>
2026-08-12 15:16:35 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,54 @@
-namespace = "com.sunglasses.linglingcalculator"
-defaultConfig {
-    applicationId = "com.sunglasses.linglingcalculator"
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sunglasses.linglingcalculator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.sunglasses.linglingcalculator"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.7.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = false 
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.bumptech.glide:Glide:4.15.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("io.coil-kt:coil:2.6.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
+    implementation(files("libs/exp4j-0.4.8.jar"))
 }
